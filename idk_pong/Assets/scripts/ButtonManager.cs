37ff892cb11d62885dd4820a2f4b0148833ad4cc
jesseleppens <jesseleppens@hotmail.com>
2021-11-02 14:12:32 +0100

    removed pong, added button

---
+++ idk_pong/Assets/scripts/ButtonManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    int n;
+    public Text buttonText;
+
+    public void OnButtonPress()
+    {
+        n++;
+        buttonText.text = n.ToString();
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
