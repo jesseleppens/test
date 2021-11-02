67f7b0622213f1092ffa7ca26242d4f8f18b3c35
jesseleppens <jesseleppens@hotmail.com>
2021-11-02 23:41:28 +0100

    added button radial

---
+++ idk_pong/Assets/scripts/ButtonAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    public Animator Animator;
+    int toggle = -1;
+
+    public void ButtonClick()
+    {
+        toggle *= -1;
+        if (toggle == 1)
+        {
+            Animator.Play("NavOpen");
+        }
+        else
+        {
+            Animator.Play("NavClose");
+        }
+    }
+}
